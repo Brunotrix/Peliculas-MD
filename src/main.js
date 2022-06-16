eaba14c51a35bb0cb90ec474859cd09f7e1fdfe8
Bruno Salinas <bruno1salinas@gmail.com>
2022-06-15 21:24:34 -0500

    Código 2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-console.log("Hola, mundo")
+async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+  
+    const movies = data.results;
+    movies.forEach(movie => {
+      const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+      
+      const movieContainer = document.createElement('div');
+      movieContainer.classList.add('movie-container');
+  
+      const movieImg = document.createElement('img');
+      movieImg.classList.add('movie-img');
+      movieImg.setAttribute('alt', movie.title);
+      movieImg.setAttribute(
+        'src',
+        'https://image.tmdb.org/t/p/w300' + movie.poster_path,
+      );
+  
+      movieContainer.appendChild(movieImg);
+      trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+  }
+  
+  getTrendingMoviesPreview();
